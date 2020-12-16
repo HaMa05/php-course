bad8c62f371879917259632c0d0bdd3188280af3
HaMa <vanha.jungle@gmail.com>
2020-12-16 19:15:38 +0700

    add conditional

--- index.php
+++ index.php
@@ -8,14 +8,17 @@
     $name = "Bui Van Ha";
     $age = 19;
 
-    $product['car'] = "BWM";
-    $product['tree'] = 'violet';
-    $product['pen'] = 'penchi';
+    $product['car'] = 20;
+    $product['tree'] = 18;
+    $product['pen'] = 19;
 
     echo "<h1>Welcome to ".$name."!</h1>";
     echo "<h2>I am ".$age." years old</h2>";
+
     foreach ($product as $item => $value) {
-        echo "<h3>".$item." is a ".$value."</h3>";
+        if($value >= $age) {
+            echo "<h3>I am ".$value." years old</h3>";
+        }
     }
 ?>
 </body>
