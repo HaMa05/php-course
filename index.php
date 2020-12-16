4c63c86ae0746153c4f08f7146379aeff1ada06c
HaMa <vanha.jungle@gmail.com>
2020-12-16 15:03:19 +0700

    use array

--- index.php
+++ index.php
@@ -8,8 +8,11 @@
     $name = "Bui Van Ha";
     $age = 19;
 
+    $arrayValue['keyframe'] = "something";
+
     echo "<h1>Welcome to ".$name."!</h1>";
     echo "<h2>I am ".$age." years old</h2>";
+    echo "<h3>I can do ".$arrayValue['keyframe']."</h3>"
 ?>
 </body>
 </html>
