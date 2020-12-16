3ef34be7d53f619cbe5bac45bc0a78f809b934cf
HaMa <vanha.jungle@gmail.com>
2020-12-16 22:40:41 +0700

    add tax calc function

--- index.php
+++ index.php
@@ -20,6 +20,20 @@
             echo "<h3>I am ".$value." years old</h3>";
         }
     }
+
+    $amount = 800;
+    $taxRate = 0.0825;
+    $addedTax = $amount * $taxRate;
+//    echo $addedTax;
+//    echo  "\n";
+g
+    function tax_calx($amount, $tax) {
+        $calulate = $amount * $tax;
+        $amount = round($amount + $calulate, 2);
+        return $amount;
+    }
+
+    echo tax_calx(750,0.233);
 ?>
 </body>
 </html>
